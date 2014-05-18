65c286bf1d4c2019b7b7b5bbec07467360bbebc5
Yuuki Furuyama <addsict@gmail.com>
2014-05-18 21:31:11 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7faf6d6..b566f49 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Data-Generator-FromDDL
    
     {{$NEXT}}
    
    +0.04 2014-05-18T12:30:52Z
    +
    +    - Changed 'INSERT INTO' statement to 'INSERT IGNORE INTO' for suppressing record duplicate error.
    +
     0.03 2014-05-04T03:56:40Z
    
         - Reduced memory usage to half.

--- lib/Data/Generator/FromDDL.pm
+++ lib/Data/Generator/FromDDL.pm
@@ -11,7 +11,7 @@ use Class::Accessor::Lite (
 use Data::Generator::FromDDL::Director;
 use Data::Generator::FromDDL::Util qw(normalize_parser_str);
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 sub generate {
     my ($self, $num, $out_fh, $format, $pretty, $bytes_per_sql) = @_;
