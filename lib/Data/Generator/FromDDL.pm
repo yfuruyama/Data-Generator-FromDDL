bfb48490610e2721180d40dc8e0801a391d7bdef
Yuuki Furuyama <addsict@gmail.com>
2014-05-22 00:25:13 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b566f49..7e28fd2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Data-Generator-FromDDL
    
     {{$NEXT}}
    
    +0.05 2014-05-21T15:24:53Z
    +
    +    - Support float, double, and boolean types.
    +
     0.04 2014-05-18T12:30:52Z
    
         - Changed 'INSERT INTO' statement to 'INSERT IGNORE INTO' for suppressing record duplicate error.

--- lib/Data/Generator/FromDDL.pm
+++ lib/Data/Generator/FromDDL.pm
@@ -11,7 +11,7 @@ use Class::Accessor::Lite (
 use Data::Generator::FromDDL::Director;
 use Data::Generator::FromDDL::Util qw(normalize_parser_str);
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 sub generate {
     my ($self, $num, $out_fh, $format, $pretty, $bytes_per_sql) = @_;
